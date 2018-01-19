5d7badd31ce96e58343b2c7535ad113fc6cff6c1
David McGrann <dpmcgrann@gmail.com>
2018-01-19 16:58:10 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -46,7 +46,7 @@ def turn(board)
   index = ""
   input_to_index(user_input)
     if valid_move?(board, index)
-      move(board, index, character)
+      puts move(board, index, character)
     else
       turn(board)
     end
